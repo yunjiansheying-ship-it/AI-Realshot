be18a31aceae770c5cc0e45caf02201503fdfc21
yunjiansheying-ship-it <yunjiansheying@gmail.com>
2026-03-06 11:53:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/AI-Realshot/',//
     const env = loadEnv(mode, '.', '');
     return {
       server: {
