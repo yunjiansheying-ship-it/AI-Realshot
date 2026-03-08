28a3593e6d3ccfa117e97e3d294e84876405d8d5
yunjiansheying-ship-it <yunjiansheying@gmail.com>
2026-03-08 21:24:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
+      base: './', //
+        define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
